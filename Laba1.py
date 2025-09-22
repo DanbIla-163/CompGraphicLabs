663cdf5c6daec383a8f9ec4efef6ae31e02bac8f
Danila <mir.dan2005@mail.ru>
2025-09-22 19:21:43 +0400

    LABA5

--- Laba1.py
+++ Laba1.py
@@ -67,8 +67,8 @@ def baricenter (x0, y0, x1, y1, x2, y2, x, y):
     lambda2 = 1.0 - lambda0 - lambda1
     return lambda0, lambda1, lambda2
 
-def draw_triangle(img_mat, z_buffer, color, polygon_texture_nums, texture_coords, texture, x0, y0, z0, x1, y1, z1, x2, y2, z2,  i0, i1, i2):
-    a = 6000
+def draw_tr_textured(img_mat, z_buffer, polygon_texture_nums, texture_coords, texture, x0, y0, z0, x1, y1, z1, x2, y2, z2, i0, i1, i2):
+    a = 6000 * coef
     px0, py0 = a * x0 / z0 + widh / 2, a * y0 / z0 + high / 2
     px1, py1 = a * x1 / z1 + widh / 2, a * y1 / z1 + high / 2
     px2, py2 = a * x2 / z2 + widh / 2, a * y2 / z2 + high / 2
@@ -115,6 +115,44 @@ def draw_triangle(img_mat, z_buffer, color, polygon_texture_nums, texture_coords
                                 texture_coords[UVt3 - 1][0]))]
                     img_mat[y][x] = color * -Intence
                     z_buffer[y][x] = z_coord
+
+def draw_triangle(img_mat, z_buff, x0, y0, z0, x1, y1, z1, x2, y2, z2, i0, i1, i2):
+    a = 10000 * coef
+    px0, py0 = a * x0 / z0 + widh / 2, a * y0 / z0 + high / 2
+    px1, py1 = a * x1 / z1 + widh / 2, a * y1 / z1 + high / 2
+    px2, py2 = a * x2 / z2 + widh / 2, a * y2 / z2 + high / 2
+
+    I0 = i0[2]
+    I1 = i1[2]
+    I2 = i2[2]
+
+    xmin = min(px0, px1, px2)
+    xmax = max(px0, px1, px2)
+    ymin = min(py0, py1, py2)
+    ymax = max(py0, py1, py2)
+    if (xmin < 0): xmin = 0
+    if (ymin < 0): ymin = 0
+    if (xmax > widh): xmax = widh
+    if (ymax > high): ymax = high
+
+    xmax = math.ceil(xmax)
+    ymax = math.ceil(ymax)
+    xmin = math.floor(xmin)
+    ymin = math.floor(ymin)
+
+    for x in range (xmin, xmax):
+        for y in range (ymin, ymax):
+            l0, l1, l2 = baricenter(px0,py0,px1,py1,px2,py2, x, y)
+            if (l0 >= 0 and l1>= 0 and l2 >= 0):
+                z_cord = l0*z0 + l1*z1 + l2*z2
+                if z_cord > z_buff[y][x]:
+                    continue
+                else:
+                    color = l0 * I0 + l1 * I1 + l2 * I2
+                    color *= -255
+                    img_mat[y][x] = color
+                    z_buff[y][x] = z_cord
+
 
 def normal(x0, y0, z0, x1, y1, z1, x2, y2, z2):
     return np.cross([x1 - x2, y1 - y2, z1 - z2], [x1 - x0, y1 - y0, z1 - z0])
--- Parser.py
+++ Parser.py
@@ -1,58 +1,91 @@
 import numpy as np
-from PIL import  Image, ImageOps
+from PIL import Image, ImageOps
 import Laba1 as gc
+import quaternions as quat
 
-f = open("model_1.obj")
-texture = np.array(ImageOps.flip(Image.open("bunny-atlas.jpg")))
+f = open("12268_banjofrog_v1_L3.obj")
+IMGTexture_exist = False
+VTexture_exist = False
+texture_img = None
 
-vectorv = []
-vectorf = []
-texture_coords = []
-texture_nums = []
-for line in f:
-    v = line.split()
+try:
+    texture_img = Image.open("12268_banjofrog_diffuse.jpg")
+    IMGTexture_exist = True
+except Exception as e:
+    print(f"Ошибка загрузки изображения: {e}")
+    IMGTexture_exist = False
 
-    if (v[0] == "vt"):
-        texture_coords.append([float(v[1]), float(v[2])])
-
-    if (v[0] == "v"):
-        vectorv.append([float(v[1]), float(v[2]), float(v[3])])
-    if (v[0] == "f"):
-        v1 = v[1].split('/')[0]
-        v2 = v[2].split('/')[0]
-        v3 = v[3].split('/')[0]
-        th1 = v[1].split('/')[1]
-        th2 = v[2].split('/')[1]
-        th3 = v[3].split('/')[1]
-        vectorf.append([int(v1), int(v2), int(v3)])
-        texture_nums.append([int(th1), int(th2), int(th3)])
-
-        img_mat2 = np.zeros(shape=(gc.high, gc.widh, 3), dtype=np.uint8)
+# Инициализация img_mat2 и z_buff_mat вне условия
+img_mat2 = np.zeros(shape=(gc.high, gc.widh, 3), dtype=np.uint8)
 z_buff_mat = np.full((gc.high, gc.widh), np.inf, dtype=np.float64)
-vectorv = gc.resize(vectorv, 0, -0.05, 0.5 , np.radians(0), np.radians(90), np.radians(0))
-normalsArr = np.zeros((len(vectorv), 3))
-normalsArr = gc.get_point_normals(vectorv, vectorf, normalsArr)
 
-for i in range(0,len(vectorf)):
-    x0 = ((vectorv[vectorf[i][0] - 1][0]))
-    y0 = ((vectorv[vectorf[i][0] - 1][1]))
-    z0 = ((vectorv[vectorf[i][0] - 1][2]))
-    x1 = ((vectorv[vectorf[i][1] - 1][0]))
-    y1 = ((vectorv[vectorf[i][1] - 1][1]))
-    z1 = ((vectorv[vectorf[i][1] - 1][2]))
-    x2 = ((vectorv[vectorf[i][2] - 1][0]))
-    y2 = ((vectorv[vectorf[i][2] - 1][1]))
-    z2 = ((vectorv[vectorf[i][2] - 1][2]))
+if IMGTexture_exist and texture_img is not None:
+    texture = np.array(ImageOps.flip(texture_img))
 
-    polygon_texture_nums = texture_nums[i]
+for times in range(1):
+    model = open("12268_banjofrog_v1_L3.obj")
+    vectorv = []
+    vectorf = []
+    texture_coords = []
+    texture_nums = []
 
-    color = (200 * gc.sekator_nelic_gran(x0, y0, z0, x1, y1, z1, x2, y2, z2), 100, 200)
-    if (gc.sekator_nelic_gran(x0, y0, z0, x1, y1, z1, x2, y2, z2) < 0):
-        gc.draw_triangle(img_mat2, z_buff_mat, color, polygon_texture_nums, texture_coords, texture, x0, y0, z0, x1, y1, z1,
-                   x2, y2, z2, normalsArr[vectorf[i][0] - 1], normalsArr[vectorf[i][1] - 1],
-                   normalsArr[vectorf[i][2] - 1])
+    for line in model:
+        v = line.split()
+        if len(v) == 0:
+            continue
+        if v[0] == "vt":
+            texture_coords.append([float(v[1]), float(v[2])])
+            VTexture_exist = True
+        if v[0] == "v":
+            vectorv.append([float(v[1]), float(v[2]), float(v[3])])
+        if v[0] == "f":
+            for i in range(2, len(v) - 1):
+                v1 = v[1].split('/')[0]
+                v2 = v[i].split('/')[0]
+                v3 = v[i + 1].split('/')[0]
+                th1 = v[1].split('/')[1]
+                th2 = v[i].split('/')[1]
+                th3 = v[i + 1].split('/')[1]
+                vectorf.append([int(v1), int(v2), int(v3)])
+                texture_nums.append([int(th1), int(th2), int(th3)])
 
+    # Преобразование меша
+    vectorv = quat.transform_mesh(
+        vectorv,
+        translation=(15 - 5 * times, 0, 30 - times),
+        euler_angles=(np.radians(90), np.radians(100), np.radians(150)),
+        scale=1
+    )
+
+    normalsArr = np.zeros((len(vectorv), 3))
+    normalsArr = gc.get_point_normals(vectorv, vectorf, normalsArr)
+
+    for i in range(len(vectorf)):
+        x0 = vectorv[vectorf[i][0] - 1][0]
+        y0 = vectorv[vectorf[i][0] - 1][1]
+        z0 = vectorv[vectorf[i][0] - 1][2]
+        x1 = vectorv[vectorf[i][1] - 1][0]
+        y1 = vectorv[vectorf[i][1] - 1][1]
+        z1 = vectorv[vectorf[i][1] - 1][2]
+        x2 = vectorv[vectorf[i][2] - 1][0]
+        y2 = vectorv[vectorf[i][2] - 1][1]
+        z2 = vectorv[vectorf[i][2] - 1][2]
+
+        polygon_texture_nums = texture_nums[i]
+
+        color = (200 * gc.sekator_nelic_gran(x0, y0, z0, x1, y1, z1, x2, y2, z2), 100, 200)
+
+        if gc.sekator_nelic_gran(x0, y0, z0, x1, y1, z1, x2, y2, z2) < 0:
+            if IMGTexture_exist and VTexture_exist:
+                gc.draw_tr_textured(img_mat2, z_buff_mat, polygon_texture_nums, texture_coords, texture,
+                                    x0, y0, z0, x1, y1, z1, x2, y2, z2,
+                                    normalsArr[vectorf[i][0] - 1], normalsArr[vectorf[i][1] - 1],
+                                    normalsArr[vectorf[i][2] - 1])
+            else:
+                gc.draw_triangle(img_mat2, z_buff_mat, x0, y0, z0, x1, y1, z1, x2, y2, z2,
+                                 normalsArr[vectorf[i][0] - 1], normalsArr[vectorf[i][1] - 1],
+                                 normalsArr[vectorf[i][2] - 1])
 
 img = Image.fromarray(img_mat2, mode="RGB")
 img = ImageOps.flip(img)
-img.save("img.jpg")
+img.save("frog.jpg")
